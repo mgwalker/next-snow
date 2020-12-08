1e6dbcd2fc475e90505435a1e1237d8f074e41ef
Greg Walker <mgwalker@gmail.com>
2020-12-07 22:45:02 -0600

    add vue; add light error handling

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,10 @@
+const appState = {
+  day: null,
+  forecast: null,
+  loading: false,
+  snowflakes: [],
+};
+
 const getNextSnow = async (forecasts) => {
   const snows = forecasts.filter(({ forecast }) =>
     forecast.toLowerCase().includes("snow")
@@ -25,25 +32,6 @@ const getForecastURL = async (latitude, longitude) => {
 };
 
 const makeItSnow = () => {
-  const getStyle = (left, size, rotation, top) => {
-    const style = {
-      position: "fixed",
-      top: `${top}px`,
-      left: `${left}px`,
-      width: `${size}px`,
-      height: `${size}px`,
-      background: "url(flake2.svg)",
-      "background-size": "contain",
-      "background-position": "center center",
-      "background-repeat": "no-repeat",
-      transform: `rotate(${rotation}deg)`,
-    };
-
-    return Object.entries(style)
-      .map(([key, value]) => `${key}: ${value}`)
-      .join(";");
-  };
-
   const createSnowflake = () => {
     const { innerWidth } = window;
 
@@ -52,39 +40,41 @@ const makeItSnow = () => {
     const rotation = Math.round(Math.random() * 90);
     const size = Math.round(Math.random() * 32) + 12;
 
-    const img = document.createElement("div");
-    img.setAttribute("data-speed", 8 + Math.floor(Math.random() * 16));
-    img.setAttribute("data-left", left);
-    img.setAttribute("data-rotation", rotation);
-    img.setAttribute("data-size", size);
-    img.setAttribute("style", getStyle(left, size, rotation, -size));
-
-    document.body.appendChild(img);
+    return {
+      speed: 8 + Math.floor(Math.random() * 16),
+      style: {
+        position: "fixed",
+        top: `${top}px`,
+        left: `${left}px`,
+        width: `${size}px`,
+        height: `${size}px`,
+        background: "url(flake2.svg)",
+        "background-size": "contain",
+        "background-position": "center center",
+        "background-repeat": "no-repeat",
+        transform: `rotate(${rotation}deg)`,
+      },
+      top: -size,
+    };
   };
 
   const moveFlakes = () => {
     const { innerHeight } = window;
-    const flakes = Array.from(document.querySelectorAll("[data-speed]"));
 
-    flakes.forEach((flake) => {
-      const top = flake.offsetTop;
+    appState.snowflakes.forEach((flake, i) => {
+      const top = flake.top;
       if (top > innerHeight) {
-        flake.remove();
-        createSnowflake();
+        appState.snowflakes.splice(i, 1, createSnowflake());
         return;
       }
 
-      const left = +flake.getAttribute("data-left");
-      const rotation = +flake.getAttribute("data-rotation");
-      const size = +flake.getAttribute("data-size");
-      const speed = +flake.getAttribute("data-speed");
-
-      flake.setAttribute("style", getStyle(left, size, rotation, top + speed));
+      flake.top += flake.speed;
+      flake.style.top = `${flake.top}px`;
     });
   };
 
   [...Array(50)].forEach(() => {
-    createSnowflake();
+    appState.snowflakes.push(createSnowflake());
   });
 
   setInterval(() => {
@@ -96,6 +86,8 @@ const updateDOMWithLocation = async (latitude, longitude) => {
   const nextSnow = await getForecastURL(latitude, longitude)
     .then(getForecast)
     .then(getNextSnow);
+  ``;
+  appState.loading = false;
 
   if (nextSnow !== false) {
     const { name, forecast } = nextSnow;
@@ -105,23 +97,50 @@ const updateDOMWithLocation = async (latitude, longitude) => {
       document.body.setAttribute("class", "day");
     }
 
-    document.getElementById("name").innerText = name;
-    document.getElementById("forecast").innerText = forecast;
+    appState.day = name;
+    appState.forecast = forecast;
     makeItSnow();
   } else {
-    document.getElementById("name").innerText = "No snow in the forecast 😢";
+    appState.day = "No snow in the forecast 😢";
   }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  // return;
+const getLocation = () => {
+  appState.day = null;
+  appState.forecast = null;
+  appState.loading = true;
   const g = navigator.geolocation;
+
   g.getCurrentPosition(
     async ({ coords: { latitude, longitude } }) => {
       updateDOMWithLocation(latitude, longitude);
     },
     ({ code }) => {
-      console.log(code);
+      appState.loading = false;
+      appState.day = "";
+      appState.forecast = "";
+
+      switch (code) {
+        case 1:
+          appState.day = "location denied by user";
+          break;
+        case 2:
+        case 3:
+        default:
+          appState.forecast =
+            "There was an error getting your location. Going to try again...";
+          setTimeout(getLocation, 3000);
+          break;
+      }
     }
   );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#container",
+    data: appState,
+  });
+  document.getElementById("container").removeAttribute("style");
+  getLocation();
 });
